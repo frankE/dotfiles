557de72c50197afa49d16983192cd71350e5d289
frankE <136004+frankE@users.noreply.github.com>
2017-10-30 08:24:27 +0100

    Moved dotfiles into home subdir

--- link.sh
+++ link.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-find .* -maxdepth 0 -type f | while read file; do rm "$HOME/$file"; ln -s "`pwd`/$file" "$HOME/$file"; done
-rm $HOME/.vim -r
-ln -s `pwd`/.vim $HOME/.vim
+cd home
+find * -maxdepth 0 | while read file; do rm "$HOME/.$file"; ln -s "`pwd`/$file" "$HOME/.$file"; done
