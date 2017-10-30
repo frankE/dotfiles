cb72e2145867065da07a7dc2c180f90a4f37ddbc
frankE <136004+frankE@users.noreply.github.com>
2017-10-30 08:52:52 +0100

    Renamed dotfiles again.

--- link.sh
+++ link.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 cd home
-find * -maxdepth 0 | while read file; do rm "$HOME/.$file"; ln -s "`pwd`/$file" "$HOME/.$file"; done
+find .* -maxdepth 0 -name '.[^.]*' | while read file; do rm "$HOME/$file"; ln -s "`pwd`/$file" "$HOME/$file"; done
+find * -maxdepth 0 | while read file; do rm "$HOME/$file"; ln -s "`pwd`/$file" "$HOME/$file"; done
