ed66c45013145be5e05be9c761b58211256c3dbc
frankE <136004+frankE@users.noreply.github.com>
2017-10-30 12:49:33 +0100

    Dir change

--- link.sh
+++ link.sh
@@ -2,5 +2,7 @@
 cd home
 find .* -maxdepth 0 -name '.[^.]*' | while read file; do rm "$HOME/$file"; ln -s "`pwd`/$file" "$HOME/$file"; done
 find * -maxdepth 0 | while read file; do rm "$HOME/$file"; ln -s "`pwd`/$file" "$HOME/$file"; done
+
+cd ..
 git submodule init
 git submodule update --recursive
