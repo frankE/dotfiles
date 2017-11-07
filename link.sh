b81a8c2898df73f2dc7981bf574be33be43a0702
frankE <136004+frankE@users.noreply.github.com>
2017-11-07 04:05:05 +0100

    Added an edit of the axiomd xfwm theme

--- link.sh
+++ link.sh
@@ -4,5 +4,8 @@ find .* -maxdepth 0 -name '.[^.]*' | while read file; do rm "$HOME/$file"; ln -s
 find * -maxdepth 0 | while read file; do rm "$HOME/$file"; ln -s "`pwd`/$file" "$HOME/$file"; done
 
 cd ..
+mkdir "$HOME/.themes"
+ln -s "`pwd`/themes/axiomd-edit" "$HOME/.themes/"
+
 git submodule init
 git submodule update --recursive
