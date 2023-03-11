1c2246024b4b92825b672a28a6744f40ac3bcdbc
FrankE <136004+frankE@users.noreply.github.com>
2023-03-11 03:19:39 +0100

    up and download tools dependencies

---
+++ fedora-install.sh
@@ -0,0 +1,13 @@
+# sudo dnf updateinfo
+# sudo dnf install git
+# sudo dnf install nvim tmux zsh  
+# sudo dnf install powerline-fonts
+# sudo dnf install progress
+# sudo dnf install python3-fabric
+# sudo dnf install python3-pyyaml
+# sudo dnf install fzf
+# curl -fLo ~/.local/bin/starship.tar.gz --create-dirs \
+#   https://github.com/starship/starship/releases/latest/download/starship-x86_64-unknown-linux-musl.tar.gz
+# tar -C ~/.local/bin -xaf ~/.local/bin/starship.tar.gz
+# rm ~/.local/bin/starship.tar.gz
+
