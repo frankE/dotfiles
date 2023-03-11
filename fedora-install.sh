bdeecf6725cedfe163d88de9b52894f730e1e9c1
FrankE <136004+frankE@users.noreply.github.com>
2023-03-11 03:41:24 +0100

    interception-tools

--- fedora-install.sh
+++ fedora-install.sh
@@ -6,6 +6,22 @@
 # sudo dnf install python3-fabric
 # sudo dnf install python3-pyyaml
 # sudo dnf install fzf
+#
+# # interception-tools (caps2esc)
+# sudo dnf copr enable fszymanski/interception-tools
+# sudo dnf install interception-tools
+# git clone https://gitlab.com/interception/linux/plugins/caps2esc.git ~/src/vendor/caps2esc
+# mkdir ~/src/vendor/caps2esc/build
+# cd ~/src/vendor/caps2esc/build
+# cmake ..
+# make -j 8
+# sudo cp caps2esc /usr/local/bin
+# cd - 
+# sudo mkdir /etc/interception
+# sudo cp udevmon.yaml /etc/interception
+# sudo systemctl enable udevmon.service
+# # End interception-tools
+#
 # curl -fLo ~/.local/bin/starship.tar.gz --create-dirs \
 #   https://github.com/starship/starship/releases/latest/download/starship-x86_64-unknown-linux-musl.tar.gz
 # tar -C ~/.local/bin -xaf ~/.local/bin/starship.tar.gz
