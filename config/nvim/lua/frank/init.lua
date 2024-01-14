03e12481bd74e6fac9d2a8e94dba24d7a0700696
FrankE <136004+frankE@users.noreply.github.com>
2024-01-14 08:40:29 +0100

    debugging stuff

--- config/nvim/lua/frank/init.lua
+++ config/nvim/lua/frank/init.lua
@@ -279,10 +279,11 @@ local dap = require('dap')
 vim.fn.sign_define('DapBreakpoint', { text = '🐞' })
 
 -- Start debugging session
+--vim.keymap.set("n", "<leader>ds", ":RustLsp debuggables<CR>")
 vim.keymap.set("n", "<leader>ds", function()
-  -- require('rustaceanvim.dap').start({workspaceRoot = vim.fn.getcwd(), cargoArgs = {}, cargoExtraArgs = {}})
-  dapui.toggle({})
-  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<C-w>=", false, true, true), "n", false) -- Spaces buffers evenly
+  vim.cmd("RustLsp debuggables")
+  -- dapui.toggle({})
+  --vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<C-w>=", false, true, true), "n", false) -- Spaces buffers evenly
 end)
 
 -- Set breakpoints, get variable values, step into/out of functions, etc.
@@ -292,6 +293,8 @@ vim.keymap.set("n", "<leader>db", dap.toggle_breakpoint)
 vim.keymap.set("n", "<leader>dn", dap.step_over)
 vim.keymap.set("n", "<leader>di", dap.step_into)
 vim.keymap.set("n", "<leader>do", dap.step_out)
+vim.keymap.set("n", "<leader>dt", dapui.toggle)
+vim.keymap.set("n", "<leader>k", dapui.eval)
 vim.keymap.set("n", "<leader>dC", function()
   dap.clear_breakpoints()
   require("notify")("Breakpoints cleared", "warn")
@@ -299,8 +302,8 @@ end)
 -- 
 ---- Close debugger and clear breakpoints
 vim.keymap.set("n", "<leader>de", function()
-  dap.clear_breakpoints()
-  dapui.toggle({})
+  --dap.clear_breakpoints()
+  -- dapui.toggle({})
   dap.terminate()
-  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<C-w>=", false, true, true), "n", false)
+  -- vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<C-w>=", false, true, true), "n", false)
 end)
