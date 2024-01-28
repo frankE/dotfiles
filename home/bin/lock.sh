1a32dfb8c98a7043c2a6c950a64e9f7f4302def7
FrankE <136004+frankE@users.noreply.github.com>
2024-01-28 02:26:30 +0100

    shebang

--- home/bin/lock.sh
+++ home/bin/lock.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 grim -o $(hyprctl -j monitors | jq -r '.[] | select(.focused) | .name') /tmp/screen.png
 
 convert /tmp/screen.png -scale 25% -blur 0x4 -scale 400% -fill black -colorize 50% /tmp/screen.png
