5124a51687eea3cffc3950f509ad7ad1e5123908
FrankE <136004+frankE@users.noreply.github.com>
2024-01-28 16:53:39 +0100

    nvim colors style=darker

--- config/nvim/lua/frank/init.lua
+++ config/nvim/lua/frank/init.lua
@@ -3,24 +3,13 @@ require("frank.set")
 require("frank.remap")
 
 -- colorscheme
--- Lua
 require('onedark').setup {
   colors = {
     green = "#eeeeee",
   },
-  style = 'darker'
+  style = 'dark'
 }
 require('onedark').load()
-function ColorWithTransparentBG(color)
-	color = color or "onedark"
-	vim.cmd.colorscheme(color)
-
-	-- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	-- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-	-- vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "none" })
-end
-
-ColorWithTransparentBG()
 
 -- fugitive
 vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
