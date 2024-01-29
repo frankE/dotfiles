29f7fea5848170f7b101f304c7e528c9d0b78f01
FrankE <136004+frankE@users.noreply.github.com>
2024-01-29 04:57:44 +0100

    blurred screen for each monitor

--- home/bin/lock.sh
+++ home/bin/lock.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
-LOCKCMD='swaylock -s fill -c 000000'
-IMAGES=''
-hyprctl -j monitors | jq -r '.[] | select(.focused) |.name' | ( while read line ; do
+LOCKCMD='swaylock'
+CMD=$(hyprctl -j monitors | jq -r '.[] | .name' | ( while read line ; do
     grim -o "$line" "/tmp/$line.png"
     convert "/tmp/$line.png" -scale 25% -blur 0x4 -scale 400% -fill black -colorize 50% "/tmp/$line.png"
     [[ -f $1 ]] && convert "/tmp/$line.png" $1 -gravity center -composite -matte "/tmp/$line.png"
-    IMAGES="$IMAGES -i \"$line\":\"/tmp/$line.png\""
+    IMAGES="$IMAGES -i $line:/tmp/$line.png"
 done
 CMD="$LOCKCMD $IMAGES"
-$CMD )
+echo $CMD))
+$CMD 
