26766136d17ebbfedf256c08fbe478888dcef6fc
FrankE <136004+frankE@users.noreply.github.com>
2024-01-31 14:18:39 +0100

    waybar config

---
+++ config/waybar/playerstatus.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+playerctl metadata -F -f '{"text": "{{emoji(volume)}} {{xesam:title}}", "tooltip": "<big>{{xesam:artist}}</big>\n{{xesam:title}}\n{{duration(position)}} / {{duration(mpris:length)}}", "class": "{{lc(status)}}", "percentage": {{volume}}}'
+
