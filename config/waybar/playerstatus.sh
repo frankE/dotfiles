1c5e2128d47d98d9185de76881c027713e1e65c5
FrankE <136004+frankE@users.noreply.github.com>
2024-02-01 04:22:53 +0100

    display / control spotify player only

--- config/waybar/playerstatus.sh
+++ config/waybar/playerstatus.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-playerctl metadata -F -f '{"text": "{{emoji(volume)}} {{xesam:title}}", "tooltip": "<big>{{xesam:artist}}</big>\n{{xesam:title}}\n{{duration(position)}} / {{duration(mpris:length)}}", "class": "{{lc(status)}}", "percentage": {{volume}}}'
+playerctl metadata -p spotify -F -f '{"text": "{{emoji(volume)}} {{xesam:title}}", "tooltip": "<big>{{xesam:artist}}</big>\n{{xesam:title}}\n{{duration(position)}} / {{duration(mpris:length)}}", "class": "{{lc(status)}}", "percentage": {{volume}}}'
 
