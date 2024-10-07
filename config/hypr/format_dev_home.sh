cb5439ea87ba186228bd4cf77dfbcbff92b4ce78
FrankE <136004+frankE@users.noreply.github.com>
2024-10-07 03:50:33 +0200

    hyprland config: rofi

--- config/hypr/format_dev_home.sh
+++ config/hypr/format_dev_home.sh
@@ -1,18 +1,20 @@
 #!/usr/bin/env bash
 
-bigScreen="desc:BNQ BenQ EW3270U Y1K00197019"
-smallScreen="desc:LG Electronics 24EA53"
+bigScreen="desc:Samsung Electric Company C49RG9x HNTR200541"
 builtin="eDP-1"
 
 hyprctl dispatch movetoworkspacesilent,1 firefox
-hyprctl dispatch movetoworkspacesilent,2 jetbrains-phpstorm
-hyprctl dispatch movetoworkspacesilent,2 gnome-terminal-server
-hyprctl dispatch movetoworkspacesilent,6 mattermost
-hyprctl dispatch movetoworkspacesilent,3 thunderbird 
-hyprctl dispatch movetoworkspacesilent,3 org.keepassxs.KeePassXC
-hyprctl dispatch movetoworkspacesilent,3 Spotify
+hyprctl dispatch movetoworkspacesilent,1 jetbrains-phpstorm
+hyprctl dispatch movetoworkspacesilent,1 gnome-terminal-server
+hyprctl dispatch movetoworkspacesilent,1 mattermost
+hyprctl dispatch movetoworkspacesilent,2 thunderbird 
+hyprctl dispatch movetoworkspacesilent,2 org.keepassxs.KeePassXC
+hyprctl dispatch movetoworkspacesilent,4 Spotify
+hyprctl dispatch movetoworkspacesilent,6 discord
 
-hyprctl dispatch moveworkspacetomonitor 1 $bigScreen
-hyprctl dispatch moveworkspacetomonitor 2 $bigScreen
-hyprctl dispatch moveworkspacetomonitor 3 $bigScreen
-hyprctl dispatch moveworkspacetomonitor 6 $smallScreen
+hyprctl dispatch moveworkspacetomonitor 1 1
+hyprctl dispatch moveworkspacetomonitor 2 1
+hyprctl dispatch moveworkspacetomonitor 3 1
+hyprctl dispatch moveworkspacetomonitor 4 1
+hyprctl dispatch moveworkspacetomonitor 5 1
+hyprctl dispatch moveworkspacetomonitor 6 1
