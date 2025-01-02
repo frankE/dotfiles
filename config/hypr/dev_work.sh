344aec1af87b788957374072cb4ea267a7f861de
FrankE <136004+frankE@users.noreply.github.com>
2025-01-02 03:42:56 +0100

    kitty as terminal

--- config/hypr/dev_work.sh
+++ config/hypr/dev_work.sh
@@ -3,7 +3,7 @@
 hyprctl dispatch exec [workspace 1 silent] phpstorm
 hyprctl dispatch exec [workspace 1 silent] firefox
 hyprctl dispatch exec [workspace 1 silent] flatpak run com.mattermost.Desktop
-hyprctl dispatch exec [workspace 1 silent] gnome-terminal
+hyprctl dispatch exec [workspace 1 silent] kitty
 hyprctl dispatch exec [workspace 2 silent] thunderbird 
 hyprctl dispatch exec [workspace 2 silent] keepassxc
 hyprctl dispatch exec [workspace 3 silent] flatpak run com.spotify.Client
--- config/hypr/home.sh
+++ config/hypr/home.sh
@@ -3,6 +3,6 @@
 hyprctl dispatch exec [workspace 1 silent] firefox
 hyprctl dispatch exec [workspace 2 silent] thunderbird 
 hyprctl dispatch exec [workspace 2 silent] keepassxc
-hyprctl dispatch exec [workspace 3 silent] gnome-terminal
+hyprctl dispatch exec [workspace 3 silent] kitty
 hyprctl dispatch exec [workspace 4 silent] flatpak run com.spotify.Client
 hyprctl dispatch exec [workspace 6 silent] ~/Discord/Discord
