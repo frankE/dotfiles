0c63e2d927f8ef7e68426fc5aa8dbf6ed29ed3ca
FrankE <frank@holyfuck.de>
2025-12-01 00:53:17 +0100

    archinstall

---
+++ arch-install.sh
@@ -0,0 +1,27 @@
+# sudo dnf updateinfo
+# sudo dnf install git
+# sudo pacman -S nvim tmux zsh  
+# sudo pacman -S powerline-fonts
+# sudo pacman -S progress
+# # sudo pacman -S python3-fabric
+# # sudo pacman -S python3-pyyaml
+# sudo pacman -S fzf
+# sudo pacman -S exa
+# sudo pacman -S kitty
+# sudo pacman -S openssh-askpass
+# sudo pacman -S hyprland hypridle hyprlock waybar udiskie rofi-wayland
+# #
+# # # interception-tools (caps2esc)
+# sudo pacman -S interception-caps2esc
+# # sudo mkdir /etc/interception
+# sudo cp udevmon.yaml /etc/interception/udevmon.d/
+# sudo systemctl restart udevmon.service
+# # # End interception-tools
+# #
+# curl -fLo ~/.local/bin/starship.tar.gz --create-dirs \
+#   https://github.com/starship/starship/releases/latest/download/starship-x86_64-unknown-linux-musl.tar.gz
+# tar -C ~/.local/bin -xaf ~/.local/bin/starship.tar.gz
+# rm ~/.local/bin/starship.tar.gz
+# 
+# # ln -s config/hypr/desktop.conf config/hypr/local.conf # OR
+# ln -s config/hypr/laptop.conf config/hypr/local.conf
